9b0804d4fa97ede0592d40397bfdead4382c1904
skyy_dev <skyzeduardo@gmail.com>
2023-08-30 11:51:11 -0300

    created pokemon base file

---
+++ Pokemon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appPokedex
+{
+    public class Pokemon
+    {
+		private String nome;
+
+		public String Nome
+		{
+			get { return nome; }
+			set { nome = value; }
+		}
+
+	}
+}
